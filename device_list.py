c87f055ab024ec26b87c8dac6149f3af6b111b90
rstrauss127 <33469244+rstrauss127@users.noreply.github.com>
2024-12-12 14:53:57 -0500

    Device List Helper Function
    Given the name of an input device, Returns index of input device

---
+++ device_list.py
@@ -0,0 +1,17 @@
+# Helper function to get input index for specific name
+#Get index of microphone name BLue snowball ice
+import pyaudio
+
+def list_devices():
+    p = pyaudio.PyAudio()
+    device_count = p.get_device_count()
+    blue_ice_index = -1
+
+    for i in range(0, device_count):
+        info = p.get_device_info_by_index(i)
+        if info['name'] == 'Blue Snowball iCE':
+            blue_ice_index = i
+        #print(f"Device {info['index']}: {info['name']}")
+    p.terminate()
+    #print(blue_ice_index)
+    return blue_ice_index
