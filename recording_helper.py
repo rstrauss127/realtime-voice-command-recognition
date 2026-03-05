5002208e76966a6dcf400ff2cbf830fdfe0b255f
Rebecca Strauss <33469244+rstrauss127@users.noreply.github.com>
2026-03-05 15:56:35 -0500

    Refactor audio recording setup and cleanup
    Removed unused list_devices function and updated input_device_index.

--- recording_helper.py
+++ recording_helper.py
@@ -7,29 +7,13 @@ CHANNELS = 1
 RATE = 16000
 p = pyaudio.PyAudio()
 
-# Helper function to get input index for specific name
-#Get index of microphone name BLue snowball ice
-#def list_devices():
-#    p = pyaudio.PyAudio()
-#    device_count = p.get_device_count()
-#    blue_ice_index = -1
-#
-#    for i in range(0, device_count):
-#        info = p.get_device_info_by_index(i)
-#        if info['name'] == 'Blue Snowball iCE':
-#            blue_ice_index = i
-#        #print(f"Device {info['index']}: {info['name']}")
-#    p.terminate()
-#    #print(blue_ice_index)
-#    return blue_ice_index
-
 def record_audio():
     stream = p.open(
         format=FORMAT,
         channels=CHANNELS,
         rate=RATE,
         input=True,
-        #input_device_index=2, # list_devices() Hard coding for now, see device_cnt.py for getting index of mic by name
+        input_device_index=0, # list_devices() Hard coding for now, see device_cnt.py for getting index of mic by name
         frames_per_buffer=FRAMES_PER_BUFFER
     )
 
