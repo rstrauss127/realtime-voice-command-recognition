3015350ce7c24fb9756bb25e30a4106e820cc814
Rebecca Strauss <33469244+rstrauss127@users.noreply.github.com>
2026-03-05 15:54:34 -0500

    Modify list_devices to accept device name parameter
    Refactor list_devices function to accept a device name parameter and return its index.

--- device_list.py
+++ device_list.py
@@ -2,16 +2,21 @@
 #Get index of microphone name BLue snowball ice
 import pyaudio
 
-def list_devices():
+def list_devices(device_name = ''):
     p = pyaudio.PyAudio()
     device_count = p.get_device_count()
-    blue_ice_index = -1
-
+    device_index = -1
+    
     for i in range(0, device_count):
         info = p.get_device_info_by_index(i)
-        if info['name'] == 'Blue Snowball iCE':
-            blue_ice_index = i
-        #print(f"Device {info['index']}: {info['name']}")
+        
+        # Grab index of device name
+        #if info['name'] == device_name:
+        #    device_index = i
+        
+        print(f"Device {info['index']}: {info['name']}")
     p.terminate()
-    print(blue_ice_index)
-    return blue_ice_index
+    #print(blue_ice_index)
+    return device_index
+
+list_devices()
